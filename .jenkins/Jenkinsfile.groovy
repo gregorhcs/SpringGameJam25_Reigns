c7b3bc61a5a3ddfa0b2948765157d1b8987d0dce
Daniel Schulz <faulolio@gmail.com>
2025-06-15 18:07:45 +0200

    build: linux+linux+mac

--- .jenkins/Jenkinsfile.groovy
+++ .jenkins/Jenkinsfile.groovy
@@ -32,9 +32,9 @@ pipeline {
 							FORMATTING_EXCLUDE : 'Library Assets/Plugins Packages',
 
 							// which executables to create
-							BUILD_FOR_WINDOWS : '0',
-							BUILD_FOR_LINUX : '0',
-							BUILD_FOR_MAC : '0',
+							BUILD_FOR_WINDOWS : '1',
+							BUILD_FOR_LINUX : '1',
+							BUILD_FOR_MAC : '1',
 							BUILD_FOR_WEBGL : '1',
 							BUILD_FOR_ANDROID : '0',
 
